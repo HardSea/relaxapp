76d8afac4c1cb48d6a573e9cf79bcc3f16fd91fc
HardSea <androidmen2000@gmail.com>
2021-05-17 22:57:13 +0300

    test

--- app/src/main/java/com/hillywave/calmdown/MainActivity.kt
+++ app/src/main/java/com/hillywave/calmdown/MainActivity.kt
@@ -7,6 +7,7 @@ import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+
 import android.graphics.drawable.AnimationDrawable
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
